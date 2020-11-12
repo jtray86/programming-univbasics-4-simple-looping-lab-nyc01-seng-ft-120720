ef15fa480f269ded99e0c492a46ca4e1faba834e
Jen Tracy <jenniferetracy@gmail.com>
2020-11-12 16:29:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,10 +1,10 @@
 require 'pry'
 def loop_message_five_times(string)
-  count=0
-  binding.pry
-  While count < 5 do
-    puts string
-    count +=1
+  counter=0
+ 
+  While counter < 5 do
+    puts string[counter]
+    counter +=1
   end
 end
 
