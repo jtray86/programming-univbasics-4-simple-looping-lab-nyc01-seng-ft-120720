9940b68a498be0da55ab1737702c94df0c0a4c35
Jen Tracy <jenniferetracy@gmail.com>
2020-11-12 15:36:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
-# Write your methods here
+require 'pry'
+def loop_message_five_times(array)
+  counter = 0 
+  binding.pry
+  While array[counter] do
+    puts array[counter]
+    counter +=1
+  end
+end
+
+def loop_message_n_times
+
+end
+
+def output_array
+
+end
+
+def return_string_array
+
+end
